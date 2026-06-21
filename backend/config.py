d5e362b10e269e7797ac40f37d25b436d78fd216
Mohammed Remshid R <remshid4@gmail.com>
2026-06-21 21:37:19 +0530

    Fix model paths

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,17 @@
-MODEL_PATH = "model/spam_model.pkl"
-VECTORIZER_PATH = "model/tfidf_vectorizer.pkl"
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "model",
+    "spam_model.pkl"
+)
+
+VECTORIZER_PATH = os.path.join(
+    BASE_DIR,
+    "model",
+    "tfidf_vectorizer.pkl"
+)
 
 MODEL_ACCURACY = 96.85
